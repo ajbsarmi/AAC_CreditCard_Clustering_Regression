95896e94bc7eb27e27f1d89f5d118da79383ae2a
ardensarmiento <arden_sarmiento@dlsu.edu.ph>
2024-05-26 14:41:52 +0800

    Add description of the changes you made

--- pages/DSF13app.py
+++ pages/DSF13app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Title
 st.title("Home")
 st.write("Hello World")
+
+# Create data frame
+df = pd.read_csv("../DSFC13_Sprint1/Data/cc_clean.csv")
+st.write(df.head())
